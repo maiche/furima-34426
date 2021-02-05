fd3889c302a896fecd7f9013951de6e634d09d25
maiche <m.otsuka2796@gmail.com>
2021-02-05 09:31:46 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /[\p{katakana}ー－&&[^ -~｡-ﾟ]]+/ }
+  validates :first_name_kana, presence: true, format: { with: /[\p{katakana}ー－&&[^ -~｡-ﾟ]]+/ }
+  validates :date_of_birth, presence: true
 end
