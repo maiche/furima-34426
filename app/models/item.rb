06d7911386e278b2cc9e57f2ac64816544d68d5b
maiche <m.otsuka2796@gmail.com>
2021-02-07 17:42:27 +0900

    ActiveHashモデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :shipping_date
 end
