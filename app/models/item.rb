123f1055d5b43784efb1063e158440260e93b944
maiche <m.otsuka2796@gmail.com>
2021-02-07 18:14:32 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,20 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :image
+    with_options length: { maximum: 40 } do
+      validates :title
+    end
+    with_options length: { maximum: 1000 } do
+      validates :description
+    end
+    with_options numericality: { other_than: 1 } do
+      validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_date_id
+    end
+    with_options numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999} do
+      validates :price
+    end
+  end
+
   belongs_to :user
   has_one_attached :image
 
