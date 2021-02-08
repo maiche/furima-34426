1ab7144fbbcf03ebc9c0d3f433798a8ec3e60739
maiche <m.otsuka2796@gmail.com>
2021-02-08 16:59:00 +0900

    editアクションの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :destroy]
+  before_action :set_item, only: [:edit, :show, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -17,6 +17,9 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
   end
 
   def show
