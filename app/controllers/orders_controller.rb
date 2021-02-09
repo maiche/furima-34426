581d8ced434abaa7adbe3c48d873e860a54638df
maiche <m.otsuka2796@gmail.com>
2021-02-09 20:02:17 +0900

    決済処理の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,7 @@ class OrdersController < ApplicationController
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -24,10 +25,19 @@ class OrdersController < ApplicationController
   end
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :district, :address_code, :building, :telephone).merge(item_id: params[:item_id]).merge(user_id: current_user.id)
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :district, :address_code, :building, :telephone).merge(item_id: params[:item_id]).merge(user_id: current_user.id).merge(token: params[:token])
   end
 
   def move_to_root
     redirect_to root_path if current_user.id == @item.user_id or @item.order.present?
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,9 +1,9 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :item_id, :user_id,:postal_code, :prefecture_id, :district, :address_code, :building, :telephone
+  attr_accessor :item_id, :user_id,:postal_code, :prefecture_id, :district, :address_code, :building, :telephone, :token
 
   with_options presence: true do
-    validates :item_id,:user_id
+    validates :item_id,:user_id, :token
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :district, :address_code
